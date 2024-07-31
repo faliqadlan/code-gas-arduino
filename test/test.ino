260adba9ad4489fc54b73afab6c04362dd60201d
adlansubejo <m.faliq.adlan@roketin.com>
2024-07-31 10:21:12 +0700

    adjsut test

--- test/test.ino
+++ test/test.ino
@@ -1,4 +1,4 @@
-float RL_VALUE = 10000;
+float RL_VALUE = 1000;
 
 void setup()
 {
@@ -7,12 +7,19 @@ void setup()
 
 void loop()
 {   
-    float rawAdc=  analogRead(A0);
+    // float rawAdc=  analogRead(A0);
     // float voltA0 = analogRead(A0) / 1023.0 * 5.0;
-    float Rs = RL_VALUE * ( 1023 -  rawAdc) / rawAdc;
+    // float Rs = RL_VALUE * ( 1023 -  rawAdc) / rawAdc;
     // Serial.println("volt");
-    Serial.println(rawAdc);
-    Serial.println(Rs);
+    // Serial.println(voltA0);
+    // Serial.println(Rs);
     // Serial.println(rawAdc);
+    Serial.println("-------start-------");
+    Serial.println(analogRead(A1) / 1023.0 * 5.0);
+    Serial.println(analogRead(A2) / 1023.0 * 5.0);
+    Serial.println(analogRead(A3) / 1023.0 * 5.0);
+    Serial.println(analogRead(A4) / 1023.0 * 5.0);
+    Serial.println(analogRead(A5)  / 1023.0 * 5.0);
+    Serial.println("-------end-------");
     delay(5000);
 }
