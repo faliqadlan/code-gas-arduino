7e771485c5fafdf7acbd81c0d9e644b88cafc96a
faliqadlan <faliqadlan67@gmail.com>
2025-01-02 09:37:18 +0700

    Update RTC-SD code to switch RTC library from DS1307 to DS3231 and enhance file handling

--- rtc-sd/rtc-sd.ino
+++ rtc-sd/rtc-sd.ino
@@ -5,7 +5,7 @@
 #define PIN_SPI_CS 4
 
 File myFile;
-RTC_DS1307 rtc;
+RTC_DS3231 rtc;
 char filename[20];
 
 void setup()
@@ -28,18 +28,29 @@ void setup()
             ; // don't do anything more:
     }
 
-    if (!rtc.isrunning())
+    if (rtc.lostPower())
     {
-        Serial.println(F("RTC is NOT running!"));
-        // following line sets the RTC to the date & time this sketch was compiled
+        Serial.println("RTC lost power, let's set the time!");
+        // The following line sets the RTC to the date & time this sketch was compiled
         rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+        // Uncomment the following line to set the date & time manually
+        // rtc.adjust(DateTime(2023, 10, 10, 12, 0, 0));
     }
 
     DateTime now = rtc.now();
     sprintf(filename, "%04d%02d%02d_%02d%02d%02d.txt", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+    Serial.print(F("Generated filename: "));
+    Serial.println(filename);
 
     // open file for writing
     myFile = SD.open(filename, FILE_WRITE);
+
+    if (!myFile)
+    {
+        // Try creating the file if it doesn't exist
+        Serial.println(F("Creating setup file"));
+        myFile = SD.open(filename, FILE_WRITE | O_CREAT);
+    }
 
     if (myFile)
     {
@@ -48,7 +59,7 @@ void setup()
     }
     else
     {
-        Serial.print(F("SD Card: error on opening file "));
+        Serial.print(F("SD Card: error on opening setup file "));
         Serial.println(filename);
     }
 }
@@ -57,6 +68,12 @@ void loop()
 {
     // open file for writing
     myFile = SD.open(filename, FILE_WRITE);
+
+    if (!myFile)
+    {
+        // Try creating the file if it doesn't exist
+        myFile = SD.open(filename, FILE_WRITE | O_CREAT);
+    }
 
     if (myFile)
     {
