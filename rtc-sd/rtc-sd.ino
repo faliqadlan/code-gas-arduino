46f6a7a3366cdd838075a1a6d7b4f90d3c9cf36d
faliqadlan <faliqadlan67@gmail.com>
2025-01-02 09:12:55 +0700

    Refactor RTC-SD code to improve SD card initialization and logging functionality

--- rtc-sd/rtc-sd.ino
+++ rtc-sd/rtc-sd.ino
@@ -1,69 +1,85 @@
+#include <SD.h>
 #include <Wire.h>
 #include <RTClib.h>
-#include <SD.h>
 
+#define PIN_SPI_CS 4
+
+File myFile;
 RTC_DS1307 rtc;
+char filename[20];
 
 void setup()
 {
     Serial.begin(9600);
-    // Initialize RTC
-    if (!rtc.begin())
+
+    if (!SD.begin(PIN_SPI_CS))
     {
-        Serial.println("Couldn't find RTC");
+        Serial.println(F("SD CARD FAILED, OR NOT PRESENT!"));
         while (1)
-            ;
+            ; // don't do anything more:
     }
 
-    // Initialize SD card
-    if (!SD.begin(4))
+    Serial.println(F("SD CARD INITIALIZED."));
+
+    if (!rtc.begin())
     {
-        Serial.println("Initialization failed!");
-        return;
+        Serial.println(F("Couldn't find RTC"));
+        while (1)
+            ; // don't do anything more:
     }
-    Serial.println("SD card initialized.");
+
+    if (!rtc.isrunning())
+    {
+        Serial.println(F("RTC is NOT running!"));
+        // following line sets the RTC to the date & time this sketch was compiled
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
+
+    DateTime now = rtc.now();
+    sprintf(filename, "%04d%02d%02d_%02d%02d%02d.txt", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+
+    // open file for writing
+    myFile = SD.open(filename, FILE_WRITE);
+
+    if (myFile)
+    {
+        myFile.println("Logger started");
+        myFile.close();
+    }
+    else
+    {
+        Serial.print(F("SD Card: error on opening file "));
+        Serial.println(filename);
+    }
 }
 
 void loop()
 {
-    // Check for "Start Monitor" action
-    if (startMonitorTriggered())
+    // open file for writing
+    myFile = SD.open(filename, FILE_WRITE);
+
+    if (myFile)
     {
-        logDateTime();
-    }
-}
-
-bool startMonitorTriggered()
-{
-    // Implement logic to detect "Start Monitor" action
-    return false; // Placeholder
-}
-
-void logDateTime()
-{
-    DateTime now = rtc.now();
-    String logFileName = "log_" + String(now.year()) + "_" + String(now.month()) + "_" + String(now.day()) + "_" + String(now.hour()) + "_" + String(now.minute()) + "_" + String(now.second()) + ".txt";
-
-    File logFile = SD.open(logFileName, FILE_WRITE);
-    if (logFile)
-    {
-        logFile.print(now.year(), DEC);
-        logFile.print('/');
-        logFile.print(now.month(), DEC);
-        logFile.print('/');
-        logFile.print(now.day(), DEC);
-        logFile.print(" ");
-        logFile.print(now.hour(), DEC);
-        logFile.print(':');
-        logFile.print(now.minute(), DEC);
-        logFile.print(':');
-        logFile.print(now.second(), DEC);
-        logFile.println();
-        logFile.close();
-        Serial.println("Logged date and time.");
+        DateTime now = rtc.now();
+        myFile.print(now.year(), DEC);
+        myFile.print('/');
+        myFile.print(now.month(), DEC);
+        myFile.print('/');
+        myFile.print(now.day(), DEC);
+        myFile.print(" ");
+        myFile.print(now.hour(), DEC);
+        myFile.print(':');
+        myFile.print(now.minute(), DEC);
+        myFile.print(':');
+        myFile.print(now.second(), DEC);
+        myFile.println(" - Logging data");
+        myFile.close();
     }
     else
     {
-        Serial.println("Error opening log file.");
+        Serial.print(F("SD Card: error on opening file "));
+        Serial.println(filename);
     }
+
+    delay(10000); // Log every 10 seconds
 }
